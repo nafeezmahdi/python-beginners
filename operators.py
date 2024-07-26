a9cb006d9a7702865d50b1b7e96dc1ef5bca29d8
nafeezmahdi <nafeezmahdi09@gmail.com>
2024-07-26 17:21:16 +0600

    operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+#  Arithmetic Operators
+# Arithmetic operators include addition(+), subtraction(-), multiplication(*), division(/), modulus(%),  (**)Exponentiation and (//)Floor division
+
+number1 = 50;
+number2= 8;
+number3= 2;
+
+exponentiation = 2**8;
+floorDiv = number1 // number2;
+
+print(exponentiation);
+print(floorDiv);
+
+# Logical Operators
+# Logical operators include and, or, not.
+print("not Operator : ", not False);
+print("not Operator : ", not (number1>number2));
+print("and Operator : ", not False and not (number1>number2));
+print("or Operator : ", not False or not (number1>number2));
+
