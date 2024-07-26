cd45a4a5c3f29f3443b88aaa2a1a728931168560
nafeezmahdi <nafeezmahdi09@gmail.com>
2024-07-26 22:07:30 +0600

    adding input-value comments

--- input-value.py
+++ input-value.py
@@ -6,3 +6,7 @@ lastName = input("Enter Your Last Name : ");
 age = int(input("Enter Your Age : "));
 fullName = firstName + " " + lastName;
 print("Name : ",fullName, " Age : ",age);
+
+# The entered value is always stored in inputString.
+# We can convert an input to an integer using int().
+# We can convert an input to a floating point using float().
