f0d49101c04122773a651d05cce2de7ba8ce0576
nafeezmahdi <nafeezmahdi09@gmail.com>
2024-07-26 22:05:41 +0600

    taking input value

---
+++ input-value.py
@@ -0,0 +1,8 @@
+# Python allows for user input.
+# That means we are able to ask the user for input.
+
+firstName = input("Enter Your First Name : ");
+lastName = input("Enter Your Last Name : ");
+age = int(input("Enter Your Age : "));
+fullName = firstName + " " + lastName;
+print("Name : ",fullName, " Age : ",age);
