540131c9144a58d24592b8d9beff2dcc5fcc35f8
nafeezmahdi <nafeezmahdi09@gmail.com>
2024-07-27 15:05:04 +0600

    strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+# In Python, a string is a sequence of characters
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+# ---> Python String Concatenation
+
+text = "This is";
+name = "Nafeez Mahdi"
+fullText = text + " " + name;
+length = len(fullText);
+
+print(fullText);
+print(length);
+
+# String concatenation means add strings together.
+# If you try to combine a string and a number, Python will give you an error.
+# To get the length of a string, use the len() function.
+
